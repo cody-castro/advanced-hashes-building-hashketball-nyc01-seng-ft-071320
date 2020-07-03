f70ce68f57aa7b60e6dd7fc3d7dbdc70e75fe0c4
Cody C <codyicastro@gmail.com>
2020-07-03 15:26:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,10 +3,10 @@ require 'pry'
 
 def game_hash
     {
-    home = {
-      team_name: "Brooklyn Nets",
-      colors: ["Black", "White"],
-      players = [
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => [
         {:player_name => "Alan Anderson", 
         :number => 0, 
         :shoe => 16, 
@@ -53,10 +53,10 @@ def game_hash
         :blocks => 11, 
         :slam_dunks => 1}
         ]},
-    away = {
-      team_name: "Charlotte Hornets",
-      colors: ["Turquoise, Purple"],
-      players = [
+    :away => {
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise, Purple"],
+      :players => [
         {:player_name => "Jeff Adrien", 
         :number => 4, 
         :shoe => 18, 
